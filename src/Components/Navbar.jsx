0475307f3519500a021f6daa97a5a0575c0bd65c
frostycakes <lamquoc.24@gmail.com>
2022-08-16 16:33:16 -0400

    Nav Logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import './navbar.scss'
+import React from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
   return (
-    <div className='navbar-parent'>
-      
+    <div className="navbar-parent">
+      <p className="logo">Quoc Lam</p>
+      <div className="nav-links">
+        <ul>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Resume</a>
+          </li>
+          <li>
+            <a href="">Contact</a>
+          </li>
+        </ul>
+        <p>Hand-coded with love</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
