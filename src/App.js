0cef041f53ac7801c724b18ba927847d10981eb1
frostycakes <lamquoc.24@gmail.com>
2022-08-16 14:22:09 -0400

    waves

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ function App() {
   return (
     <div className="App">
       <div className="background">
-        <h1>hello</h1>
+        <img className="wave3" src="../Imgs/wave3.png" alt="Wave 3" />
+        <img className="wave2" src="../Imgs/wave2.png" alt="Wave 2" />
+        <img className="wave1" src="../Imgs/wave1.png" alt="Wave 1" />
       </div>
     </div>
   );
