b6e9c9d0297767aae7ded01a1d5cc970c45a9c0a
frostycakes <lamquoc.24@gmail.com>
2022-08-17 16:05:02 -0400

    Intro Subheader Blinking Cursor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import Typed from "typed.js";
 import Navbar from "./Components/Navbar";
 
+
+//Todo: Add blinking cursor to subehader
+
 function App() {
+
+  const typedRef = useRef();
+
+  useEffect(() => {
+    const typed = new Typed(typedRef.current, {
+      strings: ["Perpetual Smiler 😁", "Continuous Learner", "Fried Chicken Enthusiast", "Problem Solver"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      backDelay: 1500,
+      loop: true,
+    });
+    return () => {
+      typed.destroy();
+    };
+  }, []);
   return (
     <div className="App">
       <div className="background">
@@ -11,9 +30,19 @@ function App() {
       </div>
       <Navbar />
       <section className="intro">
-        <p className="intro-header">Welcome to my page 👋🏼</p>
-        <p className="intro-subheader"></p>
-
+        <p className="intro-header">Hi I'm Quoc, welcome to my page! 👋🏼</p>
+        <p className="intro-subheader">
+          Food Scientist turned Software Engineer, and <span ref={typedRef}></span>
+        </p>
+        <p className="intro-text">
+          On my free time, I like long walks on the beach as much as        coding long hours into the night-- only to realize it's passed my
+          dinner time. I obsessively play puzzle games and tinker with new
+          gadgets in the kitchen. I'm enthralled by the rewarding feeling of solving a logic based coding challenge. Feel free to peruse my projects (always more to come) and have fun! 🤗
+        </p>
+        <div className="more-links">
+          {/* <a href="">More About Me</a> */}
+          <a href="">View My Resume</a>
+        </div>
       </section>
       <section className="projects"></section>
     </div>
