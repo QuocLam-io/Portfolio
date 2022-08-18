990fcac1a9e2cce62a8429e75f6be9a80f53559f
frostycakes <lamquoc.24@gmail.com>
2022-08-18 14:56:29 -0400

    projects header chips

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import Typed from "typed.js";
 import Navbar from "./Components/Navbar";
 
 function App() {
-
   const typedRef = useRef();
 
   useEffect(() => {
     const typed = new Typed(typedRef.current, {
-      strings: ["Perpetual Smiler 😁", "Continuous Learner", "Fried Chicken Enthusiast", "Problem Solver"],
+      strings: [
+        "Perpetual Smiler 😁",
+        "Continuous Learner",
+        "Fried Chicken Enthusiast",
+        "Problem Solver",
+        "Occassional Jogger",
+      ],
       typeSpeed: 50,
       backSpeed: 50,
       backDelay: 1500,
       loop: true,
     });
+
     return () => {
       typed.destroy();
     };
@@ -29,19 +35,35 @@ function App() {
       <section className="intro">
         <p className="intro-header">Hi I'm Quoc, welcome to my page! 👋🏼</p>
         <p className="intro-subheader">
-          Food Scientist turned Software Engineer, and <span ref={typedRef}></span>
+          Food Scientist turned Software Engineer, and{" "}
+          <span ref={typedRef}></span>
         </p>
         <p className="intro-text">
-          On my free time, I like long walks on the beach as much as        coding long hours into the night-- only to realize it's passed my
-          dinner time. I obsessively play puzzle games and tinker with new
-          gadgets in the kitchen. I'm enthralled by the rewarding feeling of solving a logic based coding challenge. Feel free to peruse my projects (always more to come) and have fun! 🤗
+          On my free time, I like long walks on the beach as much as coding long
+          hours into the night-- only to realize it's passed my dinner time. I
+          obsessively play puzzle games and tinker with new gadgets in the
+          kitchen. I'm enthralled by the rewarding feeling of solving a logic
+          based coding challenge. Feel free to peruse my projects (always more
+          to come) and have fun! 🤗
         </p>
         <div className="more-links">
           {/* <a href="">More About Me</a> */}
-          <a href="https://drive.google.com/file/d/12yQUTmYFx7uOEnvHbTIa49nWXuOQiGTD/view?usp=sharing" target="#" >View My Resume</a>
+          <a
+            href="https://drive.google.com/file/d/12yQUTmYFx7uOEnvHbTIa49nWXuOQiGTD/view?usp=sharing"
+            target="#"
+          >
+            View My Resume
+          </a>
         </div>
       </section>
-      <section className="projects"></section>
+      <section className="projects">
+        <header className="project-header">
+          <p>My Projects</p>
+          <img src="../Imgs/chip-html.png" alt="html chip" />
+          <img src="../Imgs/chip-css.png" alt="html chip" />
+          <img src="../Imgs/chip-js.png" alt="html chip" />
+        </header>
+      </section>
     </div>
   );
 }
