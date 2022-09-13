65b4ed1c6c51049de401254311c4f852a07a71de
frostycakes <lamquoc.24@gmail.com>
2022-09-12 22:16:35 -0400

    intro text change

--- src/App.js
+++ src/App.js
@@ -9,11 +9,9 @@ function App() {
   useEffect(() => {
     const typed = new Typed(typedRef.current, {
       strings: [
-        "Perpetual Smiler 😁",
-        "Continuous Learner",
         "Fried Chicken Enthusiast",
+        "Continuous Learner",
         "Problem Solver",
-        "Occassional Jogger",
       ],
       typeSpeed: 50,
       backSpeed: 50,
@@ -34,19 +32,21 @@ function App() {
       </div>
       <Navbar />
       <section className="intro">
-        <p className="intro-header">Hi I'm Quoc, welcome to my page! 👋🏼</p>
+        <p className="intro-header">Hi all, welcome to my page! 👋🏼</p>
         <p className="intro-subheader">
           Food Scientist turned Software Engineer, and{" "}
           <span className="cursed-blinker" ref={typedRef}></span>
         </p>
         <p className="intro-text">
-          On my free time, I like long walks on the beach as much as coding long
-          hours into the night-- only to realize it's passed my dinner time. I
-          obsessively play puzzle games and tinker with new gadgets in the
-          kitchen. I'm enthralled by the rewarding feeling of solving a logic
-          based coding challenge. Feel free to peruse my projects <span>(</span>
+          I am a Fullstack Developer, with a penchant for Frontend and a flair
+          for CSS. I'm driven by curiosity and approach everything in my life{" "}
+          <span>(</span>both professionally and personally <span>)</span> with
+          systematic quality assurance. My projects provide functionality and I
+          can proudly say, they've all been services I've always wanted to
+          build. Have fun and take a look at my projects 🤗.
+          {/* <span>(</span>
           always more to come<span>) </span>
-          and have fun! 🤗
+          and have fun! 🤗 */}
         </p>
         <div className="more-links">
           {/* <a href="">More About Me</a> */}
